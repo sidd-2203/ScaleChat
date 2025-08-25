06e486953f7151615009d906fc77f50844e5ae4f
sidd-2203 <siddharthch22@gmail.com>
2025-08-25 18:31:59 +0530

    initial commit

---
+++ apps/web/app/page.tsx
@@ -0,0 +1,22 @@
+'use client'
+import { useState } from 'react';
+import { useSocket } from '../context/SocketProvider'
+import classes from './page.module.css'
+export default function Page(){
+  const {sendMessage,messages}=useSocket();
+  const [message,setMessage]=useState('');
+  return (
+    <div>
+      
+      <div>
+        <input className={classes['chat-input']} onChange={e=>setMessage(e.target.value)} placeholder="Message...."></input>
+        <button className={classes['button']} onClick={e=>sendMessage(message)}>Send</button>
+      </div>
+      <div>
+        {messages.map((e)=>{
+          return <li>{e}</li>
+        })}
+      </div>
+    </div>
+  )
+}
